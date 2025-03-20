23945d46ee98c734a6f532ccebcb7763a09fb3fa
Sadavarsh <sadavarshsaini1@gmail.com>
2025-03-20 22:55:50 +0530

    update backend  url

--- client/src/store/shop/order-slice/index.js
+++ client/src/store/shop/order-slice/index.js
@@ -13,7 +13,7 @@ export const createNewOrder = createAsyncThunk(
   "/order/createNewOrder",
   async (orderData) => {
     const response = await axios.post(
-      "http://localhost:5000/api/shop/order/create",
+      "https://mern-ecommerce-app-zeta.vercel.app/api/shop/order/create",
       orderData
     );
 
@@ -26,7 +26,7 @@ export const capturePayment = createAsyncThunk(
   async ({ paymentId, payerId, orderId }) => {
     console.log(payerId, paymentId, orderId)
     const response = await axios.post(
-      "http://localhost:5000/api/shop/order/capture",
+      "https://mern-ecommerce-app-zeta.vercel.app/api/shop/order/capture",
       {
         paymentId,
         payerId,
@@ -42,7 +42,7 @@ export const getAllOrdersByUserId = createAsyncThunk(
   "/order/getAllOrdersByUserId",
   async (userId) => {
     const response = await axios.get(
-      `http://localhost:5000/api/shop/order/list/${userId}`
+      `https://mern-ecommerce-app-zeta.vercel.app/api/shop/order/list/${userId}`
     );
 
     return response.data;
@@ -53,7 +53,7 @@ export const getOrderDetails = createAsyncThunk(
   "/order/getOrderDetails",
   async (id) => {
     const response = await axios.get(
-      `http://localhost:5000/api/shop/order/details/${id}`
+      `https://mern-ecommerce-app-zeta.vercel.app/api/shop/order/details/${id}`
     );
 
     return response.data;
